39487b9a9501d174a838b8b5e85d8c72247fbe45
RossHoyt <rosshoytmusic@gmail.com>
2020-03-04 18:01:08 -0800

    Refactor MidiFile utility methods

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,11 @@
 /*
  ==============================================================================
- Testing MIDI File scan/reduce algorithms
+ Testing MIDI-Based scan/reduce algorithms
  ==============================================================================
  */
 
 #include <JuceHeader.h>
-#include "GeneralScanRecursive.h"
+#include "NoteHistoScan.h"
 
 const char * MIDI_FILE_REL_PATH = "/source/book1-prelude01.mid";
 //==============================================================================
@@ -14,9 +14,9 @@ const char * MIDI_FILE_REL_PATH = "/source/book1-prelude01.mid";
  * Method that reads a midi file from the provided full path.
  * @throws std::exception if file doesn't exist or MIDI file could not be read
  */
-MidiFile readMidiFile (const String &fullPath)
+static MidiFile readMidiFile (const String &relativePath)
 {
-    File fileToRead(fullPath);
+    File fileToRead(relativePath);
     if (fileToRead.existsAsFile())
     {
         MidiFile midiFile;
@@ -29,48 +29,66 @@ MidiFile readMidiFile (const String &fullPath)
     throw new std::exception;  // file doesn't exist
 }
 
-int main (int argc, char* argv[])
+/**
+ * Method that reads the NoteOn messages in a midi file into a single list
+ */
+std::vector<const MidiMessage*> getNoteList(MidiFile& midiFile, bool debug = false){
+    
+    midiFile.convertTimestampTicksToSeconds();
+    
+    // single list to store notes from all tracks
+    std::vector<const MidiMessage*> data;
+    
+    // sequentially read each track's NoteOn events into data list
+    int nTracks = midiFile.getNumTracks();
+    MidiMessageSequence* tracks = new MidiMessageSequence[nTracks];
+    for (int t = 0; t < nTracks; ++t) {
+        if(debug) DBG("Scanning track #" + std::to_string(t));
+        tracks[t] = *midiFile.getTrack(t);
+        
+        // sequentially read each note eventin track
+        int i = 0;
+        int nEvents = tracks[t].getNumEvents();
+        while (i < nEvents) {
+            MidiMessage* mm = &tracks[t].getEventPointer(i)->message;
+            if (mm->isNoteOn()) {
+                data.push_back(mm);
+                if(debug)
+                    DBG(String("Track #" + std::to_string(t)) + " @" + std::to_string(mm->getTimeStamp()) + " Note(" + std::to_string(mm->getNoteNumber()) + ")");
+            }
+            ++i;
+        }
+    }
+    delete[] tracks;
+    return data;
+}
+
+/**
+ * Static method that returns the current project directory
+ * @return String full path of the current project directory
+ */
+static String getProjectFullPath(bool debug = false)
 {
     // get current project directory
     File dir = File::getCurrentWorkingDirectory();
     for(int i = 0; i < 4; i++) dir = dir.getParentDirectory();
     String fullPath = dir.getFullPathName();
-    DBG("Current working dir = " + fullPath);
-    // append relative Midi file location to project dir
-    fullPath.append(MIDI_FILE_REL_PATH, 50);
-    
-    // read MIDI file
+    if(debug) DBG("Project full path - " + fullPath);
+    return fullPath;
+}
+
+int main (int argc, char* argv[])
+{
+    String fullPath = getProjectFullPath() + MIDI_FILE_REL_PATH;
     try{
         MidiFile midiFile = readMidiFile(fullPath);
-        
-        midiFile.convertTimestampTicksToSeconds();
-        
-        // single list to store notes from all tracks
-        std::vector<const MidiMessage*> data;
-        
-        // sequentially read each track's NoteOn events into data list
-        int nTracks = midiFile.getNumTracks();
-        MidiMessageSequence* tracks = new MidiMessageSequence[nTracks];
-        for (int t = 0; t < nTracks; ++t) {
-            DBG("Scanning track #" + std::to_string(t));
-            tracks[t] = *midiFile.getTrack(t);
-            
-            // sequentially read each note eventin track
-            int i = 0;
-            int nEvents = tracks[t].getNumEvents();
-            while (i < nEvents) {
-                MidiMessage* mm = &tracks[t].getEventPointer(i)->message;
-                if (mm->isNoteOn()) {
-                    data.push_back(mm);
-                    DBG(String("Track #" + std::to_string(t)) + " @" + std::to_string(mm->getTimeStamp()) + " Note(" + std::to_string(mm->getNoteNumber()) + ")");
-                }
-                ++i;
-            }
-        }
-        delete[] tracks;
-        return 0;
+        auto data = getNoteList(midiFile);
+        // TODO Sequential scan of data
+        // ...
     } catch(...){
-        return 1; // MIDI FILE was not read
+        DBG("MIDI File was not read");
+        return 1;
     }
+    return 0;
 }
 
