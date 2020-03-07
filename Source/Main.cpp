092da61d1357a1999318f7754c8fde92e2916054
unknown <rosshoytmusic@gmail.com>
2020-03-07 12:41:36 -0800

    Note typedef to hold noteOn and associated noteOff messages

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,44 +6,57 @@
 
 #include <JuceHeader.h>
 #include "FileUtils.h"
-
+#include "NoteHistoScan.h"
 const char * MIDI_FILE_REL_PATH = "/source/book1-prelude01.mid";
 const char * PROJECT_JUCER_FILENAME_FLAG = "Final-Project-ParAlgDev.jucer";
 
+typedef struct Note {
+    const MidiMessage * noteOn, noteOff;
+    Note(const MidiMessage* noteOn, const MidiMessage* noteOff) {
+        noteOn = noteOn;
+        noteOff = noteOff;
+    }
+};
 //=============================================================================
 
 /**
  * Method that reads the NoteOn messages in a midi file into a single list
  */
-std::vector<const MidiMessage*> getNoteList(MidiFile& midiFile, bool debug = false) {
+std::list<Note> getNoteList(MidiFile& midiFile, bool debug = false) {
+    
     midiFile.convertTimestampTicksToSeconds();
-    std::vector<const MidiMessage*> data; // list to store note events from all tracks
+    std::list<Note> data; // list to store note events from all tracks
     // Read tracks
     for (int t = 0; t < midiFile.getNumTracks(); ++t) {
         if (debug) DBG("Scanning track #" + std::to_string(t));
         auto track = *midiFile.getTrack(t);
         // copy track note events
         for(int i = 0; i < track.getNumEvents(); ++i){
-            MidiMessage* mm = &track.getEventPointer(i)->message;
-            if (mm->isNoteOn()) {
-                data.push_back(mm);
-                if (debug)
-                    DBG(String("Track #" + std::to_string(t)) + " @" + std::to_string(mm->getTimeStamp()) 
-                        + " Note(" + std::to_string(mm->getNoteNumber()) + ")");
+            MidiMessage * midiMessage = &track.getEventPointer(i)->message;
+            if (midiMessage->isNoteOn()) {
+                MidiMessage* noteOff = &track.getEventPointer(track.getIndexOfMatchingKeyUp(i))->message;
+                data.push_back(Note(midiMessage, noteOff));
+                if (debug) DBG(midiMessage->getDescription() + ", " + noteOff->getDescription());
             }
         }
     }
     return data;
 }
 
+
+//std::list<NoteHisto> sortNoteList(std::list<Note> noteList) {}
+
 int main (int argc, char* argv[])
 {
     auto fullPath = getProjectFullPath(PROJECT_JUCER_FILENAME_FLAG) + MIDI_FILE_REL_PATH;
     try{
-        auto midiFile = readInMidiFile(fullPath);
-        auto data = getNoteList(midiFile, true);
+        auto data = getNoteList(readInMidiFile(fullPath), true);
         // TODO Sequential scan of data
         // ...
+
+        for (auto noteOn : data) {
+            
+        }
         
 
     } catch(...){
